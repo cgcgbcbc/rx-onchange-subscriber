1b4158c04ecf96d1bad4419bd91322a21cd54544
cgcgbcbc <cgcgbcbc@163.com>
2015-12-07 22:23:00 +0800

    add construtor that accept observer

--- lib/src/main/java/com/cgcgbcbc/rx/OnChangeSubscriber.java
+++ lib/src/main/java/com/cgcgbcbc/rx/OnChangeSubscriber.java
@@ -37,6 +37,45 @@ public class OnChangeSubscriber<T> extends Subscriber<T> {
         this.s = new OnChangeSerializeObserver<T>(s, withFirst, equal);
     }
 
+    public OnChangeSubscriber(Observer<? super T> s) {
+        this(s, true, true, new GenericEqual<T>());
+    }
+
+    public OnChangeSubscriber(Observer<? super T> s, boolean withFirst) {
+        this(s, withFirst, true, new GenericEqual<T>());
+    }
+
+    public OnChangeSubscriber(Observer<? super T> s, boolean withFirst, boolean shareSubscriptions) {
+        this(s, withFirst, shareSubscriptions, new GenericEqual<T>());
+    }
+
+    public OnChangeSubscriber(Observer<? super T> s, Equal<T> equal) {
+        this(s, true, true, equal);
+    }
+
+    public OnChangeSubscriber(Observer<? super T> s, boolean withFirst, Equal<T> equal) {
+        this(s, withFirst, true, equal);
+    }
+
+    public OnChangeSubscriber(final Observer<? super T> s, boolean withFirst, boolean shareSubscriptions, Equal<T> equal) {
+        this(new Subscriber<T>() {
+            @Override
+            public void onCompleted() {
+                s.onCompleted();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                s.onError(e);
+            }
+
+            @Override
+            public void onNext(T o) {
+                s.onNext(o);
+            }
+        }, withFirst, shareSubscriptions, equal);
+    }
+
     @Override
     public void onCompleted() {
         s.onCompleted();
